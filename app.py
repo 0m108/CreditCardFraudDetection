bac699275718cae10eb357c73ff7634063530728
0m108 <ombhattreal@gmail.com>
2024-04-15 16:14:51 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from src.CreditCardFraudDetection.logger import logging
 from src.CreditCardFraudDetection.exception import CustomException
 import sys
 
+from src.CreditCardFraudDetection.components.data_ingestion import DataIngestionConfig,DataIngestion
+
 if __name__ == "__main__":
     logging.info("The execution has started")
     
     try:
-        a=1/0
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+        
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
--- src/CreditCardFraudDetection/components/data_ingestion.py
+++ src/CreditCardFraudDetection/components/data_ingestion.py
@@ -1,7 +1,10 @@
+from cgi import test
 import os
 import sys
 from src.CreditCardFraudDetection.exception import CustomException
 from src.CreditCardFraudDetection.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass
 
@@ -18,9 +21,21 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         try:
             ##reading the data from local storage
-            logging.info("Reading data from local storage")
+            logging.info("Reading data from local machine")
+            csv_file_path = "C:/Users/HP/Desktop/creditcard.csv"  # path of dataset in your local machine
+            df = pd.read_csv(csv_file_path)
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            train_dataset, test_dataset = train_test_split(df, test_size=0.2)
+            train_dataset.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_dataset.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
             
         except Exception as e:
             raise CustomException(e,sys)
