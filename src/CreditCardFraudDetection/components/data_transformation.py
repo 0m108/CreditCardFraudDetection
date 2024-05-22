eef4a5ed0c909071ede0e8eff6431720890c0fe2
0m108 <ombhattreal@gmail.com>
2024-05-22 07:13:49 +0530

    data transformation

--- src/CreditCardFraudDetection/components/data_transformation.py
+++ src/CreditCardFraudDetection/components/data_transformation.py
@@ -13,59 +13,17 @@ from src.CreditCardFraudDetection.logger import logging
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_object_file_path = os.path.join('artifacts','preprocessor.pkl')
-
+    preprocessor_file_path = os.path.join("artifacts","preprocessor.pkl")
+    
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config = DataTransformationConfig()
+        data_transformation_config = DataTransformationConfig()
     
-    def get_data_transformer_object(self):
-        '''
-        This function is responsible for data transformation
-        '''
-        try:
-            numerical_columns = []
-            
-            '''
-            # If categorical features are present
-            categorical_columns = []
-            '''
-            numerical_pipeline = Pipeline(steps=[
-                ("imputer", SimpleImputer(strategy='median'))
-            ])
-            '''
-            categorical_pipeline = Pipeline(steps=[
-                ("imputer", SimpleImputer(strategy = "most_frequent")),
-                ("one_hot_encoder", OneHotEncoder()),
-            ])
-            
-            logging.info(f"Categorical Columns:{categorical_columns}")
-            
-            '''
-            logging.info(f"Numerical Columns:{numerical_columns}")
-            
-            preprocessor = ColumnTransformer(
-                [
-                    ("numerical_pipeline",numerical_pipeline,numerical_columns),
-                    '''
-                    ("categorical_pipeline",categorical_pipeline,categorical_columns)
-                    '''
-                ]
-            )
-            
-            return preprocessor
-            
-        except Exception as e:
-            raise CustomException(e,sys)
-    def initiate_data_transformation(self,train_path,test_path):
-        try:
-            train_df = pd.read_csv(train_path)
-            test_df = pd.read_csv(test_path)
-            logging.info("Reading the train and test file")
-            
-            preprocessing_object = self.get_data_transformer_object()
-            
-            
-        except Exception as e:
-            raise CustomException(sys,e)
-               
+    def get_preprocessor_obj(self,train_dataset_path,test_dataset_path):
+        train_dataset = pd.read_csv(train_dataset_path)
+        test_dataset = pd.read_csv(test_dataset_path)
+        
+        
+    
+    def initiate_data_transformation(self,train_dataset_path,test_dataset_path):
+        pass
